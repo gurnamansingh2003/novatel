f63cd25d7e260755f2b515d0ba3931c02fd2d9b3
Gurnaman <gnaman1531@gmail.com>
2026-01-28 19:19:12 +0530

    portchanged

--- backend/src/config/email.config.js
+++ backend/src/config/email.config.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const createTransporter = () => {
   return nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // SSL/TLS use karega
+    port: 587,
+    secure: false, // SSL/TLS use karega
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS, // Bina space wala 16-digit code
